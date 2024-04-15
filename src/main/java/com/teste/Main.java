f4b85f5b0f5c30769bad0fbebc053a098a6cb782
victormdsm <victor.de.souza.muller@gmail.com>
2024-04-15 17:42:10 -0300

    comitando o projeto final

--- src/main/java/com/teste/Main.java
+++ src/main/java/com/teste/Main.java
@@ -1,19 +1,29 @@
-package org.example;
+package com.teste;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Pessoa p1 = new Pessoa(null, "Victor", "victor@gmail.com");
+        Pessoa p2 = new Pessoa(null, "João", "joao@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Gustavo", "Gustavo@gmail.com");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-test");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        System.out.println("Pronto");
+
+
     }
 }
